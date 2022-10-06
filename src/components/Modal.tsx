f9c62776e21473ee9fc3018e6cc1b45ab7aa5607
James Pacileo <608868+jamespacileo@users.noreply.github.com>
2022-10-06 06:04:31 +0100

    removed tab index

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -4,8 +4,6 @@ export const Modal = () => {
   return (
     <div
       id="defaultModal"
-      //   tabindex={-1}
-
       className="overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 w-full md:inset-0 h-modal md:h-full"
     >
       <div className="relative p-4 w-full max-w-2xl h-full md:h-auto">
